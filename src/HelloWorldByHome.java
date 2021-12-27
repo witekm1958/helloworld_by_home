c5728094d90e54f49f87cba28ccf3a84efde82b0
witekm1958 <45516622+witekm1958@users.noreply.github.com>
2021-12-27 13:46:46 +0100

    Dodanie wykrzykników

--- src/HelloWorldByHome.java
+++ src/HelloWorldByHome.java
@@ -1,5 +1,6 @@
 public class HelloWorldByHome {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!!!!!!!!");
+        System.out.println("Dodałem więcej wykrzykników");
     }
 }
