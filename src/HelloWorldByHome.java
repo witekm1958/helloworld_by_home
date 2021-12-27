cb5f6d56005dfa67e46685267875058c32e4a10d
Witold Matusiński <matusinski@interia.pl>
2021-12-27 23:10:03 +0100

    Dodanie programu wyświetlającego nasze imię. Modyfikacja programu HelloWorldByHome poprzez usunięcie wykrzykników i zbędnych System.out.println

--- src/HelloWorldByHome.java
+++ src/HelloWorldByHome.java
@@ -1,6 +1,5 @@
 public class HelloWorldByHome {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!!!!!!");
-        System.out.println("Dodałem więcej wykrzykników");
+        System.out.println("Hello World!");
     }
 }
---
+++ src/MyName.java
@@ -0,0 +1,6 @@
+public class MyName {
+    public static void main(String[] args) {
+        String name = "Witek";
+        System.out.println("Mam na imię " + name + ".");
+    }
+}
